bfe674390885b51ca9300807902f122e5cd7477b
UnicornFairy <unicor_n@outlook.com>
2019-02-27 21:43:14 +0800

    add 1-1code& 1-2

--- 1-1/main.cpp
+++ 1-1/main.cpp
@@ -1,7 +1,28 @@
+//C++课程练习一
+//7-1 后天 （5 分）
+/*如果今天是星期三，后天就是星期五；如果今天是星期六，后天就是星期一。我们用数字1到7对应星期一到星期日。给定某一天，请你输出那天的“后天”是星期几。
+输入格式：
+输入第一行给出一个正整数D（1 ≤ D ≤ 7），代表星期里的某一天。
+输出格式：
+在一行中输出D天的后天是星期几。
+输入样例：
+3
+输出样例：
+5
+*/
 #include <iostream>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    std::cout << "lululululu" << std::endl;
+    int D;
+    cout << "请输入一个正整数D（1 ≤ D ≤ 7），代表星期里的某一天。" << endl;
+    cin >> D;
+    if (D < 1 || D > 7) {
+        cout << "请输入正确数字。" << endl;
+    } else if (D == 5) {
+        cout << 7 << endl;
+    } else {
+        cout << (D + 2) % 7 << endl;
+    }
     return 0;
 }
