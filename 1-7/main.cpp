2425c88937910257312837d7172ddae268f01673
UnicornFairy <unicor_n@outlook.com>
2019-02-28 20:00:51 +0800

    add 1-7

--- 1-7/main.cpp
+++ 1-7/main.cpp
@@ -1,6 +1,26 @@
+/*7-7 输出个位数字
+输入一个任意整数，将其个位输出。
+输入格式:
+直接输入一个整数，没有任何附加字符。
+输出格式:
+直接输出结果，没有任何附加字符。
+输入样例:
+24
+输出样例:
+4
+*/
 #include <iostream>
 
+using namespace std;
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int x;
+    cin >> x;
+//    while(x >= 10){
+//        x = x % 10;
+//        if(x < 10)
+//            cout << x % 10 << endl;
+//    }
+    cout << x % 10 << endl;
     return 0;
 }
