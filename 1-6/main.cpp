4cabe963f25504cc487ea275c314ddcb5677d181
UnicornFairy <unicor_n@outlook.com>
2019-02-28 19:32:12 +0800

    add 1-7

--- 1-6/main.cpp
+++ 1-6/main.cpp
@@ -1,6 +1,21 @@
+/*6 两个实数的平方和
+输入两个实数，输出它们的平方和。
+输入格式:
+在一行中输入两个实数，之间用1个空格间隔。
+输出格式:
+按照“z=结果”的格式输出。
+输入样例:
+2.3 2.5
+输出样例:
+z=11.54
+*/
 #include <iostream>
 
+using namespace std;
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    double i,j;
+    cin >> i >> j;
+    cout << "z=" << (i * i) + (j * j) << endl;
     return 0;
 }
