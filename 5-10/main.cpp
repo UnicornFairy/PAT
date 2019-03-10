83acf2beb000c819135d7ee1961698736d542ba7
UnicornFairy <unicor_n@outlook.com>
2019-03-10 12:34:20 +0800

    add 5-10

---
+++ 5-10/main.cpp
@@ -0,0 +1,33 @@
+/*7-10 打印如样例所示直角三角形图案1
+从键盘输入正整数n，输出n行如样例所示直角三角形图形。
+输入格式:
+输入在一行中给出一个正整数n。
+输出格式:
+输出n行直角三角形字符阵列。格式见输出样例，每个字符前后都没有空格。
+输入样例:
+5
+输出样例:
+11111
+ 1111
+  111
+   11
+    1
+*/
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    for(int i = n; i > 0; i--){
+        for(int k = n - i; k > 0; k--){
+            cout << ' ';
+        }
+        for(int j = i; j > 0; j--){
+            cout << '1';
+        }
+        cout << endl;
+    }
+    return 0;
+}
